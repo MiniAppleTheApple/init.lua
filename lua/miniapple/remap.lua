93ce47d07c1f61f6a3435582cfa203b1aab72bcf
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-04-28 00:49:18 -0300

    remove unused part of remap.lua

--- lua/miniapple/remap.lua
+++ lua/miniapple/remap.lua
@@ -17,10 +17,5 @@ vim.keymap.set("n", "<leader>z", function()
 	end
 end)
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>rd', vim.cmd.UndotreeToggle, {})
 
