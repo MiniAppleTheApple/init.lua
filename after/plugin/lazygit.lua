822444f62fe5ec21016bcabfbbdeecbb15f19f56
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-04-28 00:49:28 -0300

    keybiding to lazygit

---
+++ after/plugin/lazygit.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_autocmd('BufEnter',  { command = ":lua require('lazygit.utils').project_root_dir()"}) 
+
+vim.keymap.set('n', '<leader>gt', ':lua require("telescope").extensions.lazygit.lazygit()<CR>')
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
+vim.keymap.set('n', '<leader>gf', ':LazyGitFilter<CR>')
+vim.keymap.set('n', '<leader>gc', ':LazyGitFilterCurrentFile<CR>')
