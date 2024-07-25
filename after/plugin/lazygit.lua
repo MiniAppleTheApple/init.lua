4865b7ba074696bf8c9926315c80c71762ba1c73
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-07-25 15:55:41 -0300

    add autocmd for VimEnter

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_create_autocmd('BufEnter',  { command = ":lua require('lazygit.utils').project_root_dir()"}) 
+vim.api.nvim_create_autocmd('VimEnter',  { command = ":Neotree"}) 
 
 vim.keymap.set('n', '<leader>gt', ':lua require("telescope").extensions.lazygit.lazygit()<CR>')
 vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
