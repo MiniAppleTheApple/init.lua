05d6a460baec15f2d57a44ef83c8dbca2ac3790b
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-07-25 16:49:49 -0300

    config zen mode

--- lua/miniapple/remap.lua
+++ lua/miniapple/remap.lua
@@ -1,7 +1,6 @@
 local zen = false
 
-vim.wo.rnu = true
-vim.wo.number = true
+
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
@@ -9,9 +8,13 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>z", function()
 	require("zen-mode").toggle()
 	if zen == false then
+		vim.wo.rnu = true
+		vim.wo.number = true
 		vim.wo.wrap = false
 		zen = true
 	else
+		vim.wo.rnu = true
+		vim.wo.number = true
 		vim.wo.wrap = true
 		zen = false
 	end
