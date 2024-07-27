2491ede7813381efd5cb908d42578c32bfc5d50b
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-07-27 00:44:12 -0300

    remove unnecessary {}

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -17,7 +17,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<leader>fs', ":Telescope themes<CR>", {})
+vim.keymap.set('n', '<leader>fs', ":Telescope themes<CR>")
 
 -- external
 vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>")
