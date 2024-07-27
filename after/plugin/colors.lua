489d731a7e71a03caee12cf6835f3f7df2504fde
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-07-27 00:33:51 -0300

    config for theme switcher

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,97 +1,27 @@
 vim.o.background = 'dark'
 
-function tokyo_night()
-	vim.cmd [[colorscheme tokyonight]]
-end
+-- sonokai 
+vim.g.sonokai_style = 'andromeda'
+vim.g.sonokai_better_performance = 1
 
-function sonokai()
-	vim.g.sonokai_style = 'andromeda'
-	vim.g.sonokai_better_performance = 1
-	vim.cmd [[colorscheme sonokai]]
-end
+-- gruvbox-material
+vim.g.gruvbox_material_deisable_italic_comment = 1
+vim.g.gruvbox_material_foreground = 'material'
+vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_better_performance = 1
+-- everforest
+vim.g.everfoest_background = 'medium'
+-- ayu
+require("ayu").setup({
+	mirage = true
+})
+-- gruvbox
+vim.g.gruvbox_italic = 1
+vim.g.gruvbox_contrast_dark = 'soft'
+-- edge
+vim.g.edge_style = 'aura'
+vim.g.edge_better_performance = 1
+-- seoul
+vim.g.seoul256_background = 234
 
-function gruvbox_material()
-	vim.g.gruvbox_material_deisable_italic_comment = 1
-	vim.g.gruvbox_material_foreground = 'material'
-	vim.g.gruvbox_material_background = 'soft'
-	vim.g.gruvbox_material_better_performance = 1
-	vim.cmd [[colorscheme gruvbox-material]]
-end
-
-function everforest()
-	vim.g.everfoest_background = 'medium'
-	vim.cmd [[colorscheme everforest]]
-end
-
-function kanagawa()
-	vim.cmd [[colorscheme kanagawa]]
-end
-
-function ayu()
-	vim.api.nvim_exec("let ayucolor='mirage'", true)
-	vim.cmd [[colorscheme ayu]]
-end
-
-function dogrun()
-	vim.cmd [[colorscheme dogrun]]
-end
-
-function solarized()
-	vim.cmd [[colorscheme solarized8]]
-end
-
-function rose_pine()
-	vim.cmd [[colorscheme rose-pine-moon]]
-end
-
-function gruvbox()
-	vim.g.gruvbox_italic = 1
-	vim.g.gruvbox_contrast_dark = 'soft'
-	vim.cmd [[colorscheme gruvbox]]
-end
-
-function one_dark_pro()
-	vim.cmd [[colorscheme onedark]]
-end
-
-function edge()
-	vim.g.edge_style = 'aura'
-        vim.g.edge_better_performance = 1
-
-	vim.cmd [[colorscheme edge]]
-end
-
-function nord()
-	vim.cmd[[colorscheme nord]]
-end
-
-function bamboo()
-	vim.cmd [[colorscheme bamboo]]
-end
-
-function night_owl()
-	vim.cmd [[colorscheme night-owl]]
-end
-
-function nordic()
-	vim.cmd [[colorscheme nordic]]
-end
-
-function seoul()
-	vim.g.seoul256_background = 234
-	vim.cmd [[colorscheme seoul256]]
-end
-
-function iceberg()
-	vim.cmd [[colorscheme iceberg]]
-end
-
-function oxocarbon()
-	vim.cmd [[colorscheme oxocarbon]]
-end
-
-function random_select(list)
-	return list[math.floor(math.random() * #list) + 1]
-end
-
-gruvbox()
+vim.cmd [[colorscheme gruvbox]]
--- after/plugin/presence.lua
+++ after/plugin/presence.lua
@@ -1,3 +1,3 @@
 require("presence").setup({
-	log_level  = "debug"
+	log_level = "debug"
 })
